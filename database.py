11c134fc1de99ce714d290eacc345634f7659308
Polina Trybialustava <polina_trybialustava@epam.com>
2026-04-20 22:58:39 +0300

    feat:hw 3-done

--- database.py
+++ database.py
@@ -24,7 +24,7 @@ params = urllib.parse.quote_plus(
 
 DATABASE_URL = f"mssql+pyodbc:///?odbc_connect={params}"
 
-SCHEMA = "DisputeSchema"
+SCHEMA = "DisputeSchema_Polina"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
